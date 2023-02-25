7b5549637f9cac50ae5da62a55c4f317b50c2b01
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-02-24 21:57:38 -0500

    fisrt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Col, Container, Row } from "reactstrap";
+import ListProduct from "./ListProduct";
+import FormProduct from './FormProduct';
 
 function App() {
+  const [pro, setPro] = useState([]);
+
+  const cargaPro = () => {
+    axios.get("http://localhost:8080/api/list").then(({ data }) => setPro(data));
+  };
+
+  useEffect(() => {
+    cargaPro();
+    const interval = setInterval(() => cargaPro(), 1 * 500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <ListProduct productos={pro} />
+        </Col>
+
+        <Col md={6}>
+         <FormProduct/>
+        </Col>
+      </Row>
+    </Container>
+  </>
   );
 }
 
